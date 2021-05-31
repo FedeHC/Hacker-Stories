ce67dc634ff8bb52f3dd00c251325848ababa445
FedeHC <fede.hc@mail.com>
2021-05-31 11:42:01 -0300

    Creando App inicial, con estilos y componentes basicos iniciales.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./img/favicon.png";
+
+
+function List(props) {
+  return (
+    <ul>
+    {props.list.map((element) => {
+      return (
+        <li key={element.objectID}>
+          <span className="fieldData">#{element.objectID}</span>
+          <br />
+
+          <span className="fieldTitle">Title: </span>
+          <span className="fieldData">{element.title}</span>
+          <br />
+
+          <span className="fieldTitle">Author/s: </span>
+          <span className="fieldData">{element.author}</span>
+          <br />
+
+          <span className="fieldTitle">URL: </span>
+          <a href={element.url} className="fieldData" target="_blank" rel="noreferrer">{element.url}</a>
+          <br /><br />
+        </li>
+      );
+    })}
+    </ul>
+  );
+}
 
 function App() {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 1,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 2,
+    },
+    {
+      title: 'Sarasa',
+      url: 'https://www.sarasa.com.ar/',
+      author: 'Sar Langa',
+      num_comments: 10,
+      points: 10,
+      objectID: 3,
+    },
+  ];
+  
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <img src={logo} className="imageLogo" alt="" />
+      <div className="divApp">
+        <h1>Lista:</h1>
+        <label htmlFor="inputSearch">Search: </label>
+        <input id="inputSearch" type="text" size={13} onChange={handleChange} />
+        <br /><br />
+        
+        <List list={stories} />
+        <br />
+
+      </div>
+    </>
   );
 }
 
