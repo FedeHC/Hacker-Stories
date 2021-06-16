2155664b1e76881c3d93044bbff8de6b7c9e7de0
FedeHC <fede.hc@mail.com>
2021-06-16 19:27:14 -0300

    Reemplazando lista no ordenada por tabla en la app.

--- src/App.js
+++ src/App.js
@@ -25,6 +25,10 @@ const useSemiPersistentState = (key, initialState = "") => {
 
 
 function App() {
+  // --------------------------------------------------------------------------
+  // Hooks
+  // --------------------------------------------------------------------------
+
   // Custom hook:
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
 
@@ -76,36 +80,34 @@ function App() {
   // useReducer:
   const [stories, dispatchStories] = useReducer(storiesReducer, reducerObject);
 
-
   // Fetch stories:
   const handleFetchStories = useCallback(async () => {
-    // Call dispatch to change to loading state:
-    dispatchStories({ type: 'STORIES_FETCH_INIT' });
-
-    // And fetch stories from HN's API:
+    dispatchStories({ type: 'STORIES_FETCH_INIT' });  // Call dispatch to change to loading state.
 
     try {
-      const result = await axios.get(url);
+      const result = await axios.get(url);            // And fetch stories from HN's API.
       dispatchStories(
         {
           type: 'STORIES_FETCH_SUCCESS',
           payload: result.data.hits,
         });
-    } catch (error) {
+    } catch (error) {                                 // Catch errors if any.
       dispatchStories(
         {
           type: 'STORIES_FETCH_FAILURE',
           payload: error.message,
         });
     }
-  }, [url]);  // Only fetch again if URL with searchTerm changes.
+  }, [url]);                                          // Only fetch again if URL with searchTerm changes.
 
   useEffect(() => {
     handleFetchStories();
-  }, [handleFetchStories]); 
+  }, [handleFetchStories]);
 
+  // --------------------------------------------------------------------------
+  // Handlers
+  // --------------------------------------------------------------------------
 
-  // Events handlers:
   const handleSearchInput = (event) => {
     setSearchTerm(event.target.value);
   };
@@ -122,43 +124,37 @@ function App() {
     });
   };
 
-  // JSX:
+  // --------------------------------------------------------------------------
+  // JSX
+  // --------------------------------------------------------------------------
   return (
     <>
       <img src={logo} className="imageLogo" alt="" />
       <div className="divApp">
         <h1>Hacker Stories</h1>
 
-        {/* Show on ERROR */}
+        {/* Show if ERROR */}
         {stories.isError &&
           <>
             <p className="subTitle">Sorry, something went wrong when searching for data. :(
-              <br />
-              Try again later.</p>
-            <br />
+            <br />Try again later.</p><br />
             {/* <p className="errorMessage">Reason: {stories.errorMsg}</p> */}
           </>
         }
 
         {/* Show while LOADING */}
-        {stories.isLoading &&
-          <p className="subTitle">Loading...</p>
-        }
+        {stories.isLoading && <p className="subTitle">Loading...</p>}
 
         {/* Show when LOADED */}
         {!stories.isLoading && !stories.isError &&
           <>
             <SearchForm searchTerm={searchTerm}
-              onSearchInput={handleSearchInput}
-              onSearchSubmit={handleSearchSubmit}
+                        onSearchInput={handleSearchInput}
+                        onSearchSubmit={handleSearchSubmit}
             />
-            <br />
-
-            <List list={stories.data}
-              className="items"
-              onRemoveItem={handleRemoveStory}
+            <TableStories list={stories.data}
+                          onRemoveItem={handleRemoveStory}
             />
-            <br />
           </>
         }
       </div>
@@ -166,105 +162,30 @@ function App() {
   );
 }
 
+// --------------------------------------------------------------------------
+// SUBCOMPONENTS
+// --------------------------------------------------------------------------
+
 const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
-  <form onSubmit={onSearchSubmit}>
-    <InputWithLabel id="searchInput"
-      value={searchTerm}
-      type="text"
-      onInputChange={onSearchInput}
-      isFocused={true}
-    >Search:</InputWithLabel>
+  <>
+    <form onSubmit={onSearchSubmit}>
+      <InputWithLabel id="searchInput"
+                      value={searchTerm}
+                      type="text"
+                      onInputChange={onSearchInput}
+                      isFocused={true}
+      >Search:</InputWithLabel>
 
-    <button id="submitButton"
-      type="submit"
-      disabled={!searchTerm}
-    >Submit!</button>
+      <button id="submitButton"
+              type="submit"
+              disabled={!searchTerm}
+      >Submit!</button>
 
-    <br /> <br />
-  </form>
+      <br /> <br />
+    </form>
+    <br />
+  </>
 );
-
-function List({ list, onRemoveItem }) {
-  return (
-    <ul>
-      {list.map((item, index) => {
-        return (
-          <li key={item.objectID} className="items">
-            <Item item={item} index={index} onRemoveItem={onRemoveItem} />
-            <br />
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
-
-function Item({ item, index, onRemoveItem }) {
-  const dateStory = new Date(item.created_at).toLocaleDateString(); // Date.
-  const timeStory = new Date(item.created_at).toLocaleTimeString(); // Time.
-  const fullDateStory = dateStory + ' ' + timeStory;                // Date+Time.
-  index = parseInt(index) + 1; // Parse index to integer and sum 1.
-
-  return (
-    <div className="containerSubItems">
-      <div className="firstColSubItems">
-        <SubItem title={'#' + index} data="" />
-      </div>
-
-      <div className="secondColSubItems">
-        <SubItem title="Title:" data={item.title} url={item.url} />
-        <SubItem title="Author/s:" data={item.author} />
-        <SubItem title="Date:" data={fullDateStory} />
-        <SubItem title="Comments:" data={item.num_comments} url={URL_COMMENTS + item.objectID} />
-        <SubItem title="Points:" data={item.points} />
-      </div>
-
-      <div className="thirdColSubItems">
-        <SubItem title="" fullItem={item} onRemoveItem={onRemoveItem} />
-      </div>
-    </div>
-  );
-}
-
-function SubItem({ title, data, isBold, url, fullItem, onRemoveItem }) {
-  return (
-    <>
-      <span className="fieldTitle">{title} </span>
-      {/* If NO LINK is given... */}
-      {!url &&
-        <span className={isBold ? "fieldDataBold" : "fieldData"}>
-          {/* If NO ITEM, just put data. */}
-          {!fullItem && data}
-
-          {/* If ITEM, insert button for dismiss story. */}
-          {fullItem &&
-            <div className="buttonDiv">
-              <button id="dissmissButton"
-                type="button"
-                onClick={() => onRemoveItem(fullItem)}>
-                ❌
-              </button>
-              <span className="buttonTooltip">Dismiss this story</span>
-            </div>
-          }
-        </span>
-      }
-
-      {/* If LINK... */}
-      {url &&
-        <>
-          <a href={url}
-            className="linkField"
-            target="_blank"
-            rel="noreferrer">
-            {data}
-          </a>
-        </>
-      }
-      <br />
-    </>
-  );
-}
 
 function InputWithLabel({ id, value, type, onInputChange, isFocused, children }) {
   // Useref hook for input:
@@ -281,11 +202,84 @@ function InputWithLabel({ id, value, type, onInputChange, isFocused, children })
     <>
       <label htmlFor={id}>{children}</label>
       <input id={id}
-        type={type}
-        size={13}
-        value={value}
-        onChange={onInputChange}
-        ref={inputRef} />
+             type={type}
+             size={13}
+             value={value}
+             onChange={onInputChange}
+             ref={inputRef} />
+    </>
+  );
+}
+
+function TableStories({ list, onRemoveItem }) {
+  return (
+    <>
+      <table className="tableStories">
+        <tbody>
+          {list.map( (item, index) => (
+            <RowStory key={item.objectID}
+                      item={item}
+                      index={index}
+                      onRemoveItem={onRemoveItem} />
+          ))}
+        </tbody>
+      </table>
+      <br/>
+    </>
+  );
+}
+
+function RowStory({ item, index, onRemoveItem }) {
+  const dateStory = new Date(item.created_at).toLocaleDateString(); // Date.
+  const timeStory = new Date(item.created_at).toLocaleTimeString(); // Time.
+  const fullDateStory = dateStory + ' ' + timeStory;                // Date+Time.
+  index = parseInt(index) + 1;                // Parse index to integer and sum 1.
+
+  return (
+    <>
+      <tr className={index % 2 === 0 ? "rowStories1" : "rowStories2"}>
+        <td className="firstCol">{'#' + index + ' '}</td>
+
+        <td className="secondCol">
+          <span className="fieldTitle">Title: </span>
+          <a href={item.url}
+             className="linkField"
+             target="_blank"
+             rel="noreferrer"
+          >{item.title}</a>
+          <br/>
+
+          <span className="fieldTitle">Author/s: </span>
+          <span className="fieldData">{item.author}</span>
+          <br/>
+
+          <span className="fieldTitle">Date: </span>
+          <span className="fieldData">{fullDateStory}</span>
+          <br/>
+
+          <span className="fieldTitle">Comments: </span>
+          <a href={URL_COMMENTS + item.objectID}
+             className="linkField"
+             target="_blank"
+             rel="noreferrer"
+          >{item.num_comments}</a>
+          <br/>
+
+          <span className="fieldTitle">Points: </span>
+          <span className="fieldData">{item.points}</span>
+          <br/>
+        </td>
+
+        <td className="thirdCol">
+          <div className="buttonDiv">
+            <button id="dissmissButton"
+                    type="button"
+                    onClick={() => onRemoveItem(item)}
+            >❌</button>
+            <span className="buttonTooltip">Dismiss this story</span>
+          </div>
+        </td>
+      </tr>
     </>
   );
 }
